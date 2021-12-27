edf41a72cc390c1d8c390bbd44aff4efe5295e9f
HopeMashal <mashal.hope@gmail.com>
2021-12-27 10:59:43 +0200

    edit router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import HomePage from './components/home';
 import FlashCard from './components/flashCard';
 import ManageCard from './components/manageCard';
@@ -9,12 +9,12 @@ import Header from './components/header';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Switch>
         <Header/>
         <Route path="/" exact component={HomePage}/>
         <Route path="/flash-card" exact component={FlashCard}/> 
         <Route path="/manage-card" exact component={ManageCard} />
-      </BrowserRouter>
+      </Switch>
     </div>
   );
 }
